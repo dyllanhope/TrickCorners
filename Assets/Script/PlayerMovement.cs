c4aef7a6e13d944e3f68eabd9f5e6c3e155be7e7
Dyllan Hope <dyllanjhope@gmail.com>
2024-06-19 08:38:54 +0100

    Implemented random spawn point
    Added code to randomly spawn a token within the playable area (within the outer wall but outside of the inner wall)

--- Assets/Script/PlayerMovement.cs
+++ Assets/Script/PlayerMovement.cs
@@ -62,7 +62,6 @@ public class PlayerMovement : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        Debug.Log("HIT");
         Destroy(gameObject);
     }
 
---
+++ Assets/Script/SpawnPointManager.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnPointManager : MonoBehaviour
+{
+    public GameObject objectToSpawn; // The object to be spawned
+    public Transform diamondCenter;  // Center of the diamonds
+    public float outerDiamondSize;   // The size of the outer diamond (side length)
+    public float innerDiamondSize;   // The size of the inner diamond (side length)
+
+    void Start()
+    {
+        SpawnObject();
+    }
+
+    void SpawnObject()
+    {
+        Vector2 randomPoint = GetRandomPointInOuterDiamond();
+        Instantiate(objectToSpawn, new Vector3(randomPoint.x, randomPoint.y, 0), Quaternion.identity);
+    }
+
+    Vector2 GetRandomPointInOuterDiamond()
+    {
+        float outerHalfSize = outerDiamondSize / 2f;
+        float innerHalfSize = innerDiamondSize / 2f;
+        Vector2 randomPoint;
+
+        do
+        {
+            // Generate a random point within the bounding box of the outer diamond
+            float randomX = Random.Range(-outerHalfSize, outerHalfSize);
+            float randomY = Random.Range(-outerHalfSize, outerHalfSize);
+            randomPoint = new Vector2(randomX, randomY);
+        }
+        // Check if the point lies within the outer diamond and outside the inner diamond
+        while (Mathf.Abs(randomPoint.x) + Mathf.Abs(randomPoint.y) > outerHalfSize || Mathf.Abs(randomPoint.x) + Mathf.Abs(randomPoint.y) < innerHalfSize);
+
+        // Transform the point to the diamond's center position
+        randomPoint += (Vector2)diamondCenter.position;
+
+        return randomPoint;
+    }
+}
